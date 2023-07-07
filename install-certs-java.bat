372e829aa5e1bad6d9329796b38a76a3f7a1ac9d
hacdescm <hacdescm@gmail.com>
2023-07-07 09:09:26 +0800

    chore: add java for win

---
+++ install-certs-java.bat
@@ -0,0 +1,5 @@
+@echo off
+
+keytool -cacerts -storepass changeit -noprompt -trustcacerts -importcert -alias ha-enterprise-ca3 -file ha-enterprise-ca3.crt 
+keytool -cacerts -storepass changeit -noprompt -trustcacerts -importcert -alias ha-root-ca1 -file ha-root-ca1.crt
+keytool -cacerts -storepass changeit -noprompt -trustcacerts -importcert -alias ha-trial-root-ca1 -file ha-trial-root-ca1.crt
